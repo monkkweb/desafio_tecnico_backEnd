bf1b4e9648d6a2f37ee758de11b5d8b824ef314e
Aurélio <marcoaureliocbarreto2008@gamil.com>
2025-03-04 12:17:45 -0400

    add mais um teste

--- src/test/java/desafio_tecnico/controller/FamiliaControllerTest.java
+++ src/test/java/desafio_tecnico/controller/FamiliaControllerTest.java
@@ -12,6 +12,11 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.ResponseEntity;
 
+import java.util.Optional;
+
+import static org.instancio.Select.field;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 class FamiliaControllerTest {
     @Mock
@@ -23,11 +28,20 @@ class FamiliaControllerTest {
     void deve_salvar_no_repositorio_os_dependentes_com_ah_renda_da_familia() {
         var dependentes = Instancio.createList(Dependente.class);
         var familia = new Familia(1L, 1200d, dependentes, 1);
-        var responseEsperado = ResponseEntity.ok(repositorio.save(familia));
 
         var response = cadastro.salvar(familia);
 
-        Assertions.assertEquals(responseEsperado, response);
+        Assertions.assertEquals(ResponseEntity.ok(repositorio.save(familia)), response);
+    }
+
+    @Test
+    void deve_retornar_a_familia_pelo_id() {
+        var familia = Instancio.of(Familia.class).set(field(Familia::getId), 1L).create();
+        when(repositorio.findById(1L)).thenReturn(Optional.of(familia));
+
+        var response = cadastro.consultaFamilia(1L);
+
+        Assertions.assertEquals(familia, response.getBody());
     }
 
 }
